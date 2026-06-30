e7973274aec96012b68064554004c08f31048cc6
StelvioMarques <stelviomarques157@gmail.com>
2026-06-30 16:47:08 +0100

    - ...

--- services/axios.ts
+++ services/axios.ts
@@ -6,7 +6,7 @@ import Cookies from "js-cookie";
 // ============ CONFIGURAÇÃO BASE ============
 
 const getBaseURL = (): string => {
-    return `https://api.faturaja.sdoca.it.ao/api`;
+    return `https://faturaja.sdoca.it.ao`;
 };
 
 const baseConfig = {
